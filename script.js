d2d343e44c152b1a1152c1f2c433d290fed42f30
Alex Scott <afscott@gmail.com>
2023-04-27 16:48:00 +0000

    fifth try, please god

--- script.js
+++ script.js
@@ -30,3 +30,32 @@ nameForm.addEventListener("submit", (event) => {
 
   result.innerHTML = `<h2>Your baby's name is: ${randomName}</h2>`;
 });
+
+async function getGPTGeneratedName(nationality1, nationality2, gender) {
+  const response = await fetch('http://localhost:3001/api/generate-name', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      nationality1,
+      nationality2,
+      gender,
+    }),
+  });
+
+  const data = await response.json();
+  return data.suggestedName;
+}
+
+nameForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  const nationality1 = document.getElementById("nationality1").value;
+  const nationality2 = document.getElementById("nationality2").value;
+  const gender = document.getElementById("gender").value;
+
+  const suggestedName = await getGPTGeneratedName(nationality1, nationality2, gender);
+
+  result.innerHTML = `<h2>Your mixed-culture baby name is: ${suggestedName}</h2>`;
+});
